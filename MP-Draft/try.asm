05575979cf053b29b957a9ac2de3dc386530207d
Faith Clarisse Sotalbo <faithsotalbo@yahoo.com>
2016-12-01 20:44:26 +0800

    Added non-working error checking, working menu

--- MP-Draft/try.asm
+++ MP-Draft/try.asm
@@ -1,18 +1,69 @@
+section .data
+STRLEN db 00
+
 section .text
 global _CheckInput
+;null, invalid, no termi, length
 
 _CheckInput:
-    
     push ebp
     mov ebp, esp
     mov eax, [ebp+8]
-    cmp eax, 01
-    jl INV
-    cmp eax, 05
-    jg INV
+    lea esi, [eax]
+    ; check if null input
+    mov dl, [esi]
+    cmp dl, 00
+    je NULL
+    ; check string length
+    mov dl, [esi+21]
+    cmp dl, 00
+    jne TOOLONG
+    ; count string length
+CHECK:
+    cmp byte [esi], 00
+    je CONTINUE
+    inc byte [STRLEN]
+    inc esi
+    jmp CHECK
+    ; check if invalid
+CONTINUE:
+    xor ecx, ecx
+    mov cl, [STRLEN]
+    dec cl
+    lea esi, [eax]
+TRAVERSE:
+    mov dl, [esi]
+    cmp dl, 0x41
+    je NEXT
+    cmp dl, 0x43
+    je NEXT
+    cmp dl, 0x47
+    je NEXT
+    cmp dl, 0x54
+    je NEXT
+    jmp INVALID
+NEXT:
+    inc esi
+    loop TRAVERSE
+    ; check if no terminator
+    inc esi
+    mov dl, [esi]
+    cmp dl, 0x2e
+    jne NOTERM
     jmp END
-INV:
-    mov eax, 11111111
-END:    
+NULL:
+    mov eax, 0x00000001
+    jmp END
+TOOLONG:
+    mov eax, 0x00000002
+    jmp END
+INVALID:
+    mov eax, 0x00000003
+    jmp END
+NOTERM:
+    mov eax, 0x00000004
+
+END:   
+    mov eax, 0xffffffff
     leave
     ret
--- MP-Draft/try.c
+++ MP-Draft/try.c
@@ -1,55 +1,120 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 extern int CheckInput();
+extern int DNALength();
+extern int ACount();
+extern int CCount();
+extern int GCount();
+extern int TCount();
+extern int *Fibonacci();
 
-void PrintMenu()
+int AskInput()
 {
-     printf("\t\t\t\tMain Menu\n");
+     int input;
+     printf("\n\t\t\t\tMain Menu\n");
      printf("\t\t1. Perform DNA Frequency Count\n");
      printf("\t\t2. Perform DNA Reverse Complement\n");
      printf("\t\t3. Perform Reverse Palindrome Test\n");
      printf("\t\t4. Perform DNA Pop Count\n");
      printf("\t\t5. Perform Genetic Fibonacci Sequence\n");
      printf("\t\t6. Exit\n");
-}   
-
-int AskInput()
-{
-     int menuInput;
-     PrintMenu();
      printf("\t\t   Enter your choice: ");
-     scanf("%d", &menuInput);
+     scanf("%d", &input);
      
-     menuInput = CheckInput(menuInput);
+     return input;
+}
+
+char *GetDNA()
+{
+     char *DNA = (char*)calloc(22, sizeof(char));
+     printf("\nEnter DNA string: ");
+     scanf("%s", DNA);
      
-     if(menuInput == -1)
-          printf("\nError: Invalid choice");
-     
-     return menuInput;
+     switch(CheckInput(DNA))
+     {
+          case -1:
+               return DNA;
+               break;
+          case 1:
+               printf("\nError: Null input");
+               return NULL;
+               break;
+          case 2:
+               printf("\nError: Beyond maximum length");
+               return NULL;
+               break;
+          case 3:
+               printf("\nError: Invalid input");
+               return NULL;
+               break;
+          case 4:
+               printf("\nError: Invalid or no terminator");
+               return NULL;
+               break;
+          default:
+               printf("\nHALA KAAAAAA");
+               return NULL;
+               break;
+     }
 }
 
 int Perform(int input)
 {
+     int month;
+     char *DNA;
+     int *FS;
      switch(input)
      {
          case 1:
-              printf("\nTask to be performed: DNA Frequency Count\n");
+              printf("\nTask to be performed: DNA Frequency Count");
+              DNA = GetDNA();
+              
+              if(DNA != NULL)
+              {
+                     /*
+                     printf("\nDNA Length: %d", DNALength(DNA));
+                     printf("\nFrequency of A: %d", ACount(DNA));
+                     printf("\nFrequency of C: %d", CCount(DNA));
+                     printf("\nFrequency of G: %d", GCount(DNA));
+                     printf("\nFrequency of T: %d", TCount(DNA));
+                     */
+                     printf("%s", DNA);
+              }
               break;
          case 2:
-              printf("\nTask to be performed: DNA Reverse Complement\n");
+              printf("\nTask to be performed: DNA Reverse Complement");
+              printf("\nEnter DNA string: ");
+              scanf("%s", DNA);
               break;
          case 3:
-              printf("\nTask to be performed: DNA Reverse Palindrome Test\n");
+              printf("\nTask to be performed: DNA Reverse Palindrome Test");
+              printf("\nEnter DNA string: ");
+              scanf("%s", DNA);
               break;
          case 4:
-              printf("\nTask to be performed: DNA POP Count\n");
+              printf("\nTask to be performed: DNA POP Count");
+              printf("\nEnter DNA string: ");
+              scanf("%s", DNA);
               break;
          case 5:
-              printf("\nTask to be performed: Genetic Fibonacci Computation\n");
+              printf("\nTask to be performed: Genetic Fibonacci Computation");
+              printf("\nEnter month: ");
+              scanf("%d", &month);
+              FS = malloc(month * sizeof(int));
+              //FS = Fibonacci(month);
+              break;
+         case 6:
+              return -1;
+              break;
+         default:
+              printf("\nError: Invalid menu choice");
+              return -1;
               break;
      }
      
-     printf("Press any key to return to main menu ...\n");
+     free(DNA);
+     printf("\nPress any key to return to main menu ...");
      getchar();
      getchar();
      return AskInput();
@@ -57,12 +122,11 @@ int Perform(int input)
 
 int main()
 {
-    int menuInput = AskInput();
+    int choice = AskInput();
     
-    while(menuInput != -1)
-    {
-        menuInput = Perform(menuInput);
-    }
+    do{
+        choice = Perform(choice);
+    }while(choice != -1);
     
     return 0;
 }
